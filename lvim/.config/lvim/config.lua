803623194bbccc0d26c5cb8403570f3ba99f1abd
Kevin7070@github <kevin.7070@gmail.com>
2024-03-28 12:33:09 -0400

    lvim color code highlight

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -47,6 +47,25 @@ lvim.plugins = {
       })
     end
   },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup {
+        swayconfig = {
+          RGB      = true, -- #RGB hex codes
+          RRGGBB   = true, -- #RRGGBB hex codes
+          names    = true, -- "Name" codes like Blue
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          rgb_fn   = true, -- CSS rgb() and rgba() functions
+          hsl_fn   = true, -- CSS hsl() and hsla() functions
+          css      = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn   = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          -- Available modes: foreground, background
+          mode     = 'background', -- Set the display mode.
+        }
+      }
+    end
+  },
 }
 
 vim.opt.relativenumber = true
@@ -97,4 +116,3 @@ local els_opts = {
   },
 }
 require("lvim.lsp.manager").setup("emmet_language_server", els_opts)
-
